b1a707c6a2fada66c278fe19c1024bdd86fd8f30
arvindpndit <arvindpandit9808@gmail.com>
2023-08-27 19:38:22 +0530

    dashboard route added

--- app/page.js
+++ app/page.js
@@ -1,7 +1,15 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main>
+    <main className="flex flex-col justify-center items-center h-screen">
       <h1 className="text-6xl text-center mt-14">HOME PAGE OF NEXTJS APP</h1>
+      <Link
+        href="/dashboard"
+        className="text-3xl mt-10 px-4 py-2 rounded-lg bg-slate-200"
+      >
+        Go the Dashboard
+      </Link>
     </main>
   );
 }
